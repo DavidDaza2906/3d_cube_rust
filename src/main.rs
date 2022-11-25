0e102444eb7395f63eebecaffff0ccf79ed0ba2a
David Daza <ddazaj@unal.edu.co>
2022-11-25 10:11:09 -0500

    added planes

--- src/main.rs
+++ src/main.rs
@@ -33,32 +33,32 @@ impl Cube {
     }
     fn projection() -> Array::<f64, Ix2>{
         let projection = array![
-            [1.0,0.0,0.0],
-            [0.0,1.0,0.0]
+            [1.,0.,0.],
+            [0.,1.,0.]
         ];
         projection
     }
     fn rotation_z(angle: &f64) -> Array::<f64, Ix2>{
         let rotation = array![
-            [angle.cos(), -angle.sin(),0.0],
-            [angle.sin(), angle.cos(),0.0],
-            [0.0,0.0,1.0]
+            [angle.cos(), -angle.sin(),0.],
+            [angle.sin(), angle.cos(),0.],
+            [0.,0.,1.]
         ];
         rotation
     }
     fn rotation_x(angle: &f64) -> Array::<f64, Ix2>{
         let rotation = array![
-            [1.0, 0.0, 0.0],
-            [0.0, angle.cos(), -angle.sin()],
-            [0.0, angle.sin(), angle.cos()]
+            [1., 0., 0.],
+            [0., angle.cos(), -angle.sin()],
+            [0., angle.sin(), angle.cos()]
         ];
         rotation
     }
     fn rotation_y(angle: &f64) -> Array::<f64, Ix2>{
         let rotation = array![
-            [angle.cos(),0.0,angle.sin()],
-            [0.0, 1.0, 0.0],
-            [-angle.sin(),0.0, angle.cos()]
+            [angle.cos(),0.,angle.sin()],
+            [0., 1., 0.],
+            [-angle.sin(),0., angle.cos()]
         ];
         rotation
     }
@@ -66,40 +66,86 @@ impl Cube {
         use graphics::*;
         let points = Cube::points();
         let mut projected : Vec<Array::<f64,Ix1>> = Vec::new();
-        const BACKGROUND_COLOR: [f32;4] = [0.0,0.0,0.0,1.0];
-        const CUBE_COLOR: [f32;4] = [0.6,0.6,1.0,1.0];
+        const BACKGROUND_COLOR: [f32;4] = [0.101,0.101,0.141,1.];
+        const LINE_COLOR: [f32;4] = [1.,1.,1.0,0.5];
+        const PANEL_COLORS: [[f32;4];6] = [
+            [0.18,0.55,0.87,0.7],
+            [0.94,0.35,0.37,0.7],
+            [0.22,0.7,0.6,0.7],
+            [0.64,0.57,0.91,0.7],
+            [0.75,0.83,0.35,0.7],
+            [0.25,0.33,0.15,0.7]
+            ];
         let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        let (x, y) = (args.window_size[0] / 2., args.window_size[1] / 2.);
         self.gl.draw(args.viewport(), |c, gl| {
             clear(BACKGROUND_COLOR, gl);
             let transform = c
                 .transform
                 .trans(x,y);
+
             for i in 0..points.shape()[0]{
                 let mut rotated = Cube::rotation_x(&rotation).dot(&points.row(i));
                 rotated = Cube::rotation_y(&rotation).dot(&rotated);
                 rotated = Cube::rotation_z(&rotation).dot(&rotated);
                 let mut projected2d = Cube::projection().dot(&rotated);
                 projected2d = self.size * projected2d;
-                let point = rectangle::square(projected2d[0],projected2d[1],10.0);
                 projected.push(projected2d);
             }
+            
+            polygon(PANEL_COLORS[0],&[
+                [projected[0][0],projected[0][1]],
+                [projected[3][0],projected[3][1]],
+                [projected[7][0],projected[7][1]],
+                [projected[4][0],projected[4][1]],
+],transform,gl);           
+            polygon(PANEL_COLORS[1],&[
+                [projected[6][0],projected[6][1]],
+                [projected[7][0],projected[7][1]],
+                [projected[3][0],projected[3][1]],
+                [projected[2][0],projected[2][1]],
+],transform,gl);
+            polygon(PANEL_COLORS[2],&[
+                [projected[5][0],projected[5][1]],
+                [projected[1][0],projected[1][1]],
+                [projected[2][0],projected[2][1]],
+                [projected[6][0],projected[6][1]],
+],transform,gl);
+            polygon(PANEL_COLORS[3],&[
+                [projected[4][0],projected[4][1]],
+                [projected[5][0],projected[5][1]],
+                [projected[6][0],projected[6][1]],
+                [projected[7][0],projected[7][1]],
+                ],transform,gl);
+            polygon(PANEL_COLORS[4],&[
+                [projected[0][0],projected[0][1]],
+                [projected[1][0],projected[1][1]],
+                [projected[2][0],projected[2][1]],
+                [projected[3][0],projected[3][1]],
+   ],transform,gl);
+            polygon(PANEL_COLORS[5],&[
+                [projected[4][0],projected[4][1]],
+                [projected[5][0],projected[5][1]],
+                [projected[1][0],projected[1][1]],
+                [projected[0][0],projected[0][1]],
+   ],transform,gl);  
+
             for i in 0..4{
                 let mut start = &projected[i];
                 let mut end = &projected[i+4];
-                line(CUBE_COLOR,1.0,[start[0],start[1],end[0],end[1]],transform,gl);
+                line(LINE_COLOR,2.0,[start[0],start[1],end[0],end[1]],transform,gl);
                 start = &projected[i];
                 end = &projected[(i+1)%4];
-                line(CUBE_COLOR,1.0,[start[0],start[1],end[0],end[1]],transform,gl);
+                line(LINE_COLOR,2.0,[start[0],start[1],end[0],end[1]],transform,gl);
                 start = &projected[i+4];
                 end = &projected[((i+1)%4)+ 4];
-                line(CUBE_COLOR,1.0,[start[0],start[1],end[0],end[1]],transform,gl);
+                line(LINE_COLOR,3.0,[start[0],start[1],end[0],end[1]],transform,gl);
 
             }
         });
     }
     fn update(&mut self, args: &UpdateArgs){
-        self.rotation += 2.0 * args.dt;
+        self.rotation += 0.9 * args.dt;
     }
 }
 
@@ -112,8 +158,8 @@ fn main() {
         .unwrap();
     let mut cube = Cube {
         gl: GlGraphics::new(opengl),
-        rotation : 0.0,
-        size: 100.0,
+        rotation : 0.,
+        size: 100.,
     };
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window){
